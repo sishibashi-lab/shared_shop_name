48c208af20a7bfac753c37f36c02d95b83e0e63e
kawashimah <kawashima.h0001@gmail.com>
2026-06-02 11:41:57 +0900

    test

--- src/main/java/jp/co/sss/shop/controller/login/LoginController.java
+++ src/main/java/jp/co/sss/shop/controller/login/LoginController.java
@@ -14,7 +14,7 @@ import jp.co.sss.shop.form.LoginForm;
 import jp.co.sss.shop.repository.UserRepository;
 import jp.co.sss.shop.util.Constant;
 
-	//test
+	//test kawashima
 
 /**
  * ログイン機能のコントローラクラス
